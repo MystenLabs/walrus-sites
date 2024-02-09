6158a5da1f8c36ca7232e14c174a23627d4c605f
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2024-02-09 16:28:38 +0100

    refactor: remove old portal directory

--- portal-v1/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
