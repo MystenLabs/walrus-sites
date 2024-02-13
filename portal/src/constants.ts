6a856d70f7ab5ee0e289c0bde41e3e185b8ba2ab
giac-mysten <giacomo@mystenlabs.com>
2024-02-13 10:44:31 +0000

    feat: improve url parsing to be hosting and regex independent
    * The conversion url -> subdomain + path does not require to specify
      the base domain
    * development on localhost is supported with a separate condition (the
      length of "subdomain.localhost".split(".") is 2)

--- portal/src/constants.ts
+++ portal/src/constants.ts
@@ -1,8 +1,4 @@
 export const NETWORK = "testnet"
-export const BASE_URL = "localhost:8080"
-export const PROTO = "http"
-// export const BASE_URL = "blocksite.net"
-// export const PROTO = "https"
 export const SITE_NAMES: { [key: string]: string } = {
     // Any hardcoded (non suins) name -> object_id mappings go here
     // e.g.,
