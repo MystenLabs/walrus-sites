6814c9d6bd76015b7ef0ff14424b5e1bc40c8050
giac-mysten <giacomo@mystenlabs.com>
2024-06-05 15:56:18 +0100

    fix: fix index page
    The div was missing, so the error message was not displayed.

--- site-builder/src/site/resource.rs
+++ site-builder/src/site/resource.rs
@@ -253,12 +253,12 @@ impl ResourceSet {
     }
 
     /// Returns a vector of operations to delete all resources in the set.
-    pub fn delete_all<'a>(&'a self) -> Vec<ResourceOp<'a>> {
+    pub fn delete_all(&self) -> Vec<ResourceOp> {
         self.inner.iter().map(ResourceOp::Deleted).collect()
     }
 
     /// Returns a vector of operations to create all resources in the set.
-    pub fn create_all<'a>(&'a self) -> Vec<ResourceOp<'a>> {
+    pub fn create_all(&self) -> Vec<ResourceOp> {
         self.inner.iter().map(ResourceOp::Created).collect()
     }
 
