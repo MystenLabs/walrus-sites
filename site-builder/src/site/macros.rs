1950a07df277fbd3b1e7c46e8f85ef6c53fe1b5f
Markus Legner <mlegner@users.noreply.github.com>
2024-06-18 13:13:36 +0200

    fix(deps): bump Sui to testnet-v1.27.2 (#74)
    Includes a workaround for a (temporarily) removed function.
    
    Also simplifies the `deny.toml` configuration.

--- site-builder/src/site/macros.rs
+++ site-builder/src/site/macros.rs
@@ -19,7 +19,17 @@ macro_rules! match_for_correct_type {
 macro_rules! get_dynamic_field {
     ($struct:expr, $field_name:expr, $field_type:path $({ $var:ident })*) => {
         match_for_correct_type!(
-            $struct.read_dynamic_field_value($field_name),
+            // TODO(mlegner): Change this back to $struct.field_value($field_name) when bumping Sui
+            // to a version that includes https://github.com/MystenLabs/sui/pull/18193.
+            match &$struct {
+                sui_sdk::rpc_types::SuiMoveStruct::WithFields(fields) => {
+                    fields.get($field_name).cloned()
+                }
+                sui_sdk::rpc_types::SuiMoveStruct::WithTypes { type_: _, fields } => {
+                    fields.get($field_name).cloned()
+                }
+                _ => None,
+            },
             $field_type $({ $var })*
         ).ok_or(anyhow!(
             "SuiMoveStruct does not contain field {} with expected type {}: {:?}",
