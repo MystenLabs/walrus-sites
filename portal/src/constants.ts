f2ed750296c9e00abad606bbeb688d56a7481b74
giac-mysten <giacomo@mystenlabs.com>
2024-06-18 12:26:55 +0100

    feat: change to official aggregator

--- portal/src/constants.ts
+++ portal/src/constants.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const NETWORK = "testnet"
-export const AGGREGATOR = "https://tm92ijl7qf.execute-api.us-west-1.amazonaws.com/AggregatorStage"
+export const AGGREGATOR = "https://aggregator-devnet.walrus.space:443"
 export const SITE_PACKAGE = "0x514cf7ce2df33b9e2ca69e75bc9645ef38aca67b6f2852992a34e35e9f907f58"
 export const MAX_REDIRECT_DEPTH = 3
 export const SITE_NAMES: { [key: string]: string } = {
