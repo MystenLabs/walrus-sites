d888a05766684f82aa9fa7a03322bd429c5abe5a
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-07-22 18:22:21 +0300

    Update portal/src/http_status_codes.ts
    Co-authored-by: giac-mysten <124184614+giac-mysten@users.noreply.github.com>

--- portal/src/http_status_codes.ts
+++ portal/src/http_status_codes.ts
@@ -3,16 +3,7 @@
 
 // Purpose: Contains the enumeration of HTTP status codes.
 export enum HttpStatusCodes {
-    OK = 200,
-    CREATED = 201,
-    NO_CONTENT = 204,
-    MOVED_PERMANENTLY = 301,
-    FOUND = 302,
     TOO_MANY_REDIRECTS = 310,
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
     NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR = 500,
     LOOP_DETECTED = 508
 }
