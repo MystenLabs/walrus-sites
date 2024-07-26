08d30a6f1b65252ce823c652b0ceb83a37651d47
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-07-26 15:30:55 +0300

    Scaffold server side portal
    Remove redundant files and dependencies
    
    Update .gitignore - exclude .next/
    
    Remove even more files
    
    Update pnpm-lock.yaml

---
+++ portal/server/app/route.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export async function GET(req: Request){
+    return new Response('Soon to be a server portal!', {status: 200, statusText: 'OK'})
+}
---
+++ portal/server/next.config.mjs
@@ -0,0 +1,7 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default nextConfig
