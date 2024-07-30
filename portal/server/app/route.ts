648d650eea10cb7c8aa28aa07d537f49a7c94623
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-07-30 14:45:03 +0300

    Refactor route.ts baseURL condition

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { NextResponse } from 'next/server'
 import { getDomain, getSubdomainAndPath } from '@lib/domain_parsing'
 import { redirectToAggregatorUrlResponse, redirectToPortalURLResponse } from '@lib/redirects'
 import { getBlobIdLink, getObjectIdLink } from '@lib/links'
 import { resolveAndFetchPage } from '@lib/page_fetching'
+import { removeLastSlash } from '@lib/domain_parsing'
 
 export async function GET(req: Request) {
     const originalUrl = req.headers.get('x-original-url')
@@ -35,13 +37,12 @@ export async function GET(req: Request) {
     }
 
     const scopeString = new URL(req.url).origin
-
+    const onBaseURL = removeLastSlash(req.url) === removeLastSlash(scopeString)
+    const onBaseURLIndex = removeLastSlash(req.url) === removeLastSlash(scopeString) + 'index.html'
     // Handle the case in which we are at the root `BASE_URL`
-    if (req.url === scopeString || req.url === scopeString + 'index.html') {
+    if ( onBaseURL || onBaseURLIndex ) {
         console.log('serving the landing page')
-        const newUrl = scopeString + 'index-sw-enabled.html'
-
-        return fetch(newUrl)
+        return // TODO: serve the landing page from /public
     }
 
     const response = await fetch(req)
