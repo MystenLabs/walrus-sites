ac174ab360e7bd05d0f9bd27aa755d7bb14b9c29
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-07 13:48:44 +0100

    fix(portal): fetch walrus sites index sw enabled (#154)
    While fetching `walrus.site/index.html` is registering a service worker,
    which
    we don't want in our server portal implementation.
    
    `index-sw-enabled.html` in the same, but without the service worker
    registration.

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -37,7 +37,7 @@ export async function GET(req: Request) {
     const atBaseUrl = portalDomain == url.host.split(':')[0]
     if (atBaseUrl) {
         console.log('serving the landing page from another service')
-        const landingPageServiceName = `https://walrus.site`
+        const landingPageServiceName = `https://walrus.site/index-sw-enabled.html`
         const response = await fetch(`${landingPageServiceName}${parsedUrl?.path}`)
         const data = await response.text()
         console.log(response.headers.get('content-type'))
