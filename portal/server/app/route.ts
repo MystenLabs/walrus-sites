00e6dcbe27395c8af6b3912dc415fbab7033aea3
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-21 20:27:26 +0300

    feat(portal): host the landing page to walrus sites (#174)
    - Create `publish_landing_page.sh` utility script to publish the landing
    page content to walrus sites.
    - Modify the case when browsing on the base url of the portal, and fetch
    the landing page from walrus sites instead of the service worker
    `walrus.site` portal.

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -1,12 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { getDomain, getSubdomainAndPath, removeLastSlash } from '@lib/domain_parsing'
+import { getDomain, getSubdomainAndPath } from '@lib/domain_parsing'
 import { redirectToAggregatorUrlResponse, redirectToPortalURLResponse } from '@lib/redirects'
 import { getBlobIdLink, getObjectIdLink } from '@lib/links'
 import { resolveAndFetchPage } from '@lib/page_fetching'
-import { NextResponse } from 'next/server'
-import path from 'path'
 
 export async function GET(req: Request) {
     const originalUrl = req.headers.get('x-original-url')
@@ -38,24 +36,18 @@ export async function GET(req: Request) {
 
     const atBaseUrl = portalDomain == url.host.split(':')[0]
     if (atBaseUrl) {
-        console.log('serving the landing page from another service')
-        const landingPageServiceName = `https://walrus.site`
-        const response = await fetch(`${landingPageServiceName}${parsedUrl?.path}`)
-        const data = await response.text()
-        console.log(response.headers.get('content-type'))
-
-        // Proxy requests for CSS and fonts
-        const proxiedData = data.replace(
-            /(href|src)="\/([^"]+)"/g,
-            `$1="${landingPageServiceName}/$2"`
-        )
-
-        // Return the fetched data as a response
-        return new Response(proxiedData, {
-            headers: {
-                'content-type': response.headers.get('content-type') ?? 'text/html'
+        console.log('Serving the landing page from walrus...')
+        const blobId = '55onty23j6xl6axb7z2o03t5zs6gmosw30qjb4lqr3t60ukc0a'
+        const resourcePath = parsedUrl?.path == '/index.html' ?
+            '/index-sw-enabled.html' :
+            parsedUrl?.path ?? '/index-sw-enabled.html'
+        const response = await resolveAndFetchPage(
+            {
+                subdomain: blobId,
+                path: resourcePath
             }
-        })
+        )
+        return response
     }
 
     return new Response(`Resource at ${originalUrl} not found!`, { status: 404 })
