4e8481c40b341e8ec335920f9e2044e2302a6b49
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-21 15:59:14 +0300

    Fix CORS error by loading the page directly from walrus

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -36,28 +36,18 @@ export async function GET(req: Request) {
 
     const atBaseUrl = portalDomain == url.host.split(':')[0]
     if (atBaseUrl) {
-        console.log('serving the landing page from walrus sites')
+        console.log('Serving the landing page from walrus...')
         const blobId = '55onty23j6xl6axb7z2o03t5zs6gmosw30qjb4lqr3t60ukc0a'
-        const landingPageURLString = `https://${blobId}.blob.store`
         const resourcePath = parsedUrl?.path == '/index.html' ?
             '/index-sw-enabled.html' :
             parsedUrl?.path ?? '/index-sw-enabled.html'
-        console.log(`fetching ${landingPageURLString}${resourcePath}`)
-        const response = await fetch(`${landingPageURLString}${resourcePath}`)
-        const data = await response.text()
-
-        // Proxy requests for CSS and fonts
-        const proxiedData = data.replace(
-            /(href|src)="\/([^"]+)"/g,
-            `$1="${landingPageURLString}/$2"`
-        )
-
-        // Return the fetched data as a response
-        return new Response(proxiedData, {
-            headers: {
-                'content-type': response.headers.get('content-type') ?? 'text/html'
+        const response = await resolveAndFetchPage(
+            {
+                subdomain: blobId,
+                path: resourcePath
             }
-        })
+        )
+        return response
     }
 
     return new Response(`Resource at ${originalUrl} not found!`, { status: 404 })
