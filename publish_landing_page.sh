0391b455e1678a8223113dfacac0c19a8954efab
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-22 10:45:14 +0300

    Rename index-sw-enabled.html upon publish landing page
    While publishing the landing page to walrus,
    the `index-sw-enabled.html` is renamed to `index.html`
    to avoid resolving this mapping on
    `portal/server/app/route.ts`.

--- publish_landing_page.sh
+++ publish_landing_page.sh
@@ -8,11 +8,9 @@ cargo build --release && \
 echo "Creating temporary landing page directory..." && \
 mkdir temp-landing-page && \
 cp -r portal/common/static/* temp-landing-page && \
-rm temp-landing-page/{\
-    index.html,\
-    404-page.template.html,\
-    sw.js,walrus-sites-portal-register-sw.js\
-    } && \
+rm temp-landing-page/index.html && \
+mv temp-landing-page/index-sw-enabled.html temp-landing-page/index.html && \
+rm temp-landing-page/{404-page.template.html,sw.js,walrus-sites-portal-register-sw.js} && \
 echo "Publishing landing page to walrus sites..."
 ./target/release/site-builder --config \
 site-builder/assets/builder-example.yaml publish temp-landing-page/ \
