7bea4e4901b0bcbb65a4b41b46cbf541ec4df731
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-29 14:25:30 +0300

    Use describe and expect

--- portal/common/lib/page_fetching.bench.ts
+++ portal/common/lib/page_fetching.bench.ts
@@ -1,15 +1,18 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { bench } from 'vitest';
+import { bench, describe, expect } from 'vitest';
 import { fetchPage } from './page_fetching';
 import { SuiClient, getFullnodeUrl } from '@mysten/sui/client'
 import { NETWORK } from './constants';
 
-bench('fetch the landing page walrus site', async () => {
-    const rpcUrl = getFullnodeUrl(NETWORK);
-    const client = new SuiClient({ url: rpcUrl });
-    const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
-    const resourcePath = '/index.html';
-    await fetchPage(client, objectId, resourcePath)
+describe('Page fetching', () => {
+    bench('fetch the landing page walrus site', async () => {
+        const rpcUrl = getFullnodeUrl(NETWORK);
+        const client = new SuiClient({ url: rpcUrl });
+        const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
+        const resourcePath = '/index.html';
+        const res = await fetchPage(client, objectId, resourcePath);
+        expect(res.status).toEqual(200); //
+    });
 });
