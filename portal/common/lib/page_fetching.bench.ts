cd60b508220855f60d172b1e443241dff9b99104
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-29 14:31:20 +0300

    Add comment about raised errors in vitest bench

--- portal/common/lib/page_fetching.bench.ts
+++ portal/common/lib/page_fetching.bench.ts
@@ -13,6 +13,6 @@ describe('Page fetching', () => {
         const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
         const resourcePath = '/index.html';
         const res = await fetchPage(client, objectId, resourcePath);
-        expect(res.status).toEqual(200); //
+        expect(res.status).toEqual(200); // If this fails, then the bench will result in 0ms
     });
 });
