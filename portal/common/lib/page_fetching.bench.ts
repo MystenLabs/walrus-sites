5ce1e6136e21de09eab42582dc592270b9562fbd
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-08-29 14:00:15 +0300

    Fix fetchPage bench - was yielding 404

--- portal/common/lib/page_fetching.bench.ts
+++ portal/common/lib/page_fetching.bench.ts
@@ -10,6 +10,6 @@ bench('fetch the landing page walrus site', async () => {
     const rpcUrl = getFullnodeUrl(NETWORK);
     const client = new SuiClient({ url: rpcUrl });
     const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
-    const resourcePath = '/';
+    const resourcePath = '/index.html';
     await fetchPage(client, objectId, resourcePath)
 });
