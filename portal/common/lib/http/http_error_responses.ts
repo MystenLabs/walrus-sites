61cacf8189b42a19e8113a20f9dc11b60e7e0c4e
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-09-09 13:54:12 +0300

    Fix missing newline in the 404 page
    The message shown would be like the
    following:
    
    "You have ... internet!Please turn back!"
    In order to add the newline, I would have
    break this sentence into two, because
    in HTML cannot be added simply
    including the "\n" character.

--- portal/common/lib/http/http_error_responses.ts
+++ portal/common/lib/http/http_error_responses.ts
@@ -5,23 +5,27 @@ import template_404 from "../../static/404-page.template.html";
 
 export function siteNotFound(): Response {
     return Response404(
-        "You have reached the end of the internet, please turn back!" +
+        "You have reached the end of the internet, please turn back!",
         "Invalid URL: The object ID is not a valid Walrus Site."
     );
 }
 
 export function noObjectIdFound(): Response {
-    return Response404("You have reached the end of the internet, please turn back!" +
-        "Invalid URL: No object ID could be found.");
+    return Response404(
+        "You have reached the end of the internet, please turn back!",
+        "Invalid URL: No object ID could be found."
+    );
 }
 
 export function fullNodeFail(): Response {
     return Response404("Failed to contact the full node.");
 }
 
-function Response404(message: String): Response {
-    console.log();
-    return new Response(template_404.replace("${message}", message), {
+function Response404(message: String, secondaryMessage?: String): Response {
+    const interpolated = template_404
+        .replace("${message}", message)
+        .replace("${secondaryMessage}", secondaryMessage ?? '')
+    return new Response(interpolated, {
         status: 404,
         headers: {
             "Content-Type": "text/html",
