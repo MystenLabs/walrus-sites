767e870c9214baa44c449d2a1cae3ad2ff24e320
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-09-10 11:11:44 +0300

    Bench flatland - fetchPage & fetchResource

--- portal/common/lib/page_fetching.bench.ts
+++ portal/common/lib/page_fetching.bench.ts
@@ -6,13 +6,20 @@ import { fetchPage } from './page_fetching';
 import { SuiClient, getFullnodeUrl } from '@mysten/sui/client'
 import { NETWORK } from './constants';
 
+const LANDING_PAGE_OID = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
+const FLATLAND_OID = '0x049b6d3f34789904efcc20254400b7dca5548ee35cd7b5b145a211f85b2532fa';
+const ws_pages = [
+    [LANDING_PAGE_OID, "landing page"],
+    [FLATLAND_OID, "flatland"]
+]
+const rpcUrl = getFullnodeUrl(NETWORK);
+const client = new SuiClient({ url: rpcUrl });
 describe('Page fetching', () => {
-    bench('fetchPage: fetch the landing page walrus site', async () => {
-        const rpcUrl = getFullnodeUrl(NETWORK);
-        const client = new SuiClient({ url: rpcUrl });
-        const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
-        const resourcePath = '/index.html';
-        const res = await fetchPage(client, objectId, resourcePath);
-        expect(res.status).toEqual(200); // If this fails, then the bench will result in 0ms
-    });
+    ws_pages.forEach(([objectId, siteName]) => {
+        bench(`fetchResource: fetch the ${siteName} walrus site`, async () => {
+            const resourcePath = '/index.html';
+            const res = await fetchPage(client, objectId, resourcePath);
+            expect(res.status).toEqual(200); // If this fails, then the bench will result in 0ms
+        });
+    })
 });
--- portal/common/lib/resource.bench.ts
+++ portal/common/lib/resource.bench.ts
@@ -5,15 +5,22 @@ import { bench, describe, expect } from 'vitest';
 import { fetchResource } from './resource';
 import { SuiClient, getFullnodeUrl } from '@mysten/sui/client'
 import { NETWORK } from './constants';
-import { isResource, isVersionedResource, VersionedResource } from './types';
+import { isVersionedResource } from './types';
 
-describe('Page fetching', () => {
-    bench('fetchResource: fetch the landing page walrus site', async () => {
-        const rpcUrl = getFullnodeUrl(NETWORK);
-        const client = new SuiClient({ url: rpcUrl });
-        const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
-        const resourcePath = '/index.html';
-        const resp = await fetchResource(client, objectId, resourcePath, new Set());
-        expect(isVersionedResource(resp)).toBeTruthy();
-    });
+const LANDING_PAGE_OID = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
+const FLATLAND_OID = '0x049b6d3f34789904efcc20254400b7dca5548ee35cd7b5b145a211f85b2532fa';
+const ws_pages = [
+    [LANDING_PAGE_OID, "landing page"],
+    [FLATLAND_OID, "flatland"]
+]
+const rpcUrl = getFullnodeUrl(NETWORK);
+const client = new SuiClient({ url: rpcUrl });
+describe('Resource fetching', () => {
+    ws_pages.forEach(([objectId, siteName]) => {
+        bench(`fetchResource: fetch the ${siteName} walrus site`, async () => {
+            const resourcePath = '/index.html';
+            const resp = await fetchResource(client, objectId, resourcePath, new Set());
+            expect(isVersionedResource(resp)).toBeTruthy();
+        });
+    })
 });
