82046b1929c44fc67e785dcaafc126036202ab7d
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-09-11 16:31:16 +0300

    fix(portal): Fix missing newline in the 404 page (#202)
    The message shown would be like the
    following:
    
    "You have ... internet!Please turn back!"
    
    In order to add the newline, I would have
    break this sentence into two, because
    in HTML a newline cannot be added simply
    including the "\n" character.
    
    ---------
    
    Co-authored-by: giac-mysten <124184614+giac-mysten@users.noreply.github.com>

--- portal/common/lib/http/http_error_responses.ts
+++ portal/common/lib/http/http_error_responses.ts
@@ -3,25 +3,31 @@
 
 import template_404 from "../../static/404-page.template.html";
 
+const mainNotFoundErrorMessage = "You have reached the end of the internet, please turn back!"
+
 export function siteNotFound(): Response {
     return Response404(
-        "You have reached the end of the internet, please turn back!" +
+        mainNotFoundErrorMessage,
         "Invalid URL: The object ID is not a valid Walrus Site."
     );
 }
 
 export function noObjectIdFound(): Response {
-    return Response404("You have reached the end of the internet, please turn back!" +
-        "Invalid URL: No object ID could be found.");
+    return Response404(
+        mainNotFoundErrorMessage,
+        "Invalid URL: No object ID could be found."
+    );
 }
 
 export function fullNodeFail(): Response {
     return Response404("Failed to contact the full node.");
 }
 
-function Response404(message: String): Response {
-    console.log();
-    return new Response(template_404.replace("${message}", message), {
+function Response404(message: String, secondaryMessage?: String): Response {
+    const interpolated = template_404
+        .replace("${message}", message)
+        .replace("${secondaryMessage}", secondaryMessage ?? '')
+    return new Response(interpolated, {
         status: 404,
         headers: {
             "Content-Type": "text/html",
