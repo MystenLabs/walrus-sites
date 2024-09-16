48b1d228eecb400ed9ce3c35e88617169776d072
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-09-16 17:26:27 +0300

    feat(portal): Bench flatland - fetchPage & fetchResource (#204)
    Bench flatland with fetchPage and fetchResource.
    
    The addition was simple: use the already existing bench files for
    fetchPage and fetchResource,
    create some abstractions to keep it DRY, and include the flatland WS
    objectId.

--- portal/common/lib/resource.bench.ts
+++ portal/common/lib/resource.bench.ts
@@ -5,15 +5,17 @@ import { bench, describe, expect } from 'vitest';
 import { fetchResource } from './resource';
 import { SuiClient, getFullnodeUrl } from '@mysten/sui/client'
 import { NETWORK } from './constants';
-import { isResource, isVersionedResource, VersionedResource } from './types';
+import { isVersionedResource } from './types';
+import { SITES_USED_FOR_BENCHING } from './constants';
 
-describe('Page fetching', () => {
-    bench('fetchResource: fetch the landing page walrus site', async () => {
-        const rpcUrl = getFullnodeUrl(NETWORK);
-        const client = new SuiClient({ url: rpcUrl });
-        const objectId = '0x5fa99da7c4af9e2e2d0fb4503b058b9181693e463998c87c40be78fa2a1ca271';
-        const resourcePath = '/index.html';
-        const resp = await fetchResource(client, objectId, resourcePath, new Set());
-        expect(isVersionedResource(resp)).toBeTruthy();
-    });
+const rpcUrl = getFullnodeUrl(NETWORK);
+const client = new SuiClient({ url: rpcUrl });
+describe('Resource fetching', () => {
+    SITES_USED_FOR_BENCHING.forEach(([objectId, siteName]) => {
+        bench(`fetchResource: fetch the ${siteName} walrus site`, async () => {
+            const resourcePath = '/index.html';
+            const resp = await fetchResource(client, objectId, resourcePath, new Set());
+            expect(isVersionedResource(resp)).toBeTruthy();
+        });
+    })
 });
