9b5ec714268884a059b4a668b780708d9ca1edf3
giac-mysten <giacomo@mystenlabs.com>
2024-10-04 12:46:57 +0100

    chore(move): upgrade to Move 2024 and publish new contract
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>

--- portal/common/lib/constants.ts
+++ portal/common/lib/constants.ts
@@ -14,3 +14,13 @@ export const SITE_NAMES: { [key: string]: string } = {
 export const FALLBACK_PORTAL = "blob.store";
 // The string representing the ResourcePath struct in the walrus_site package.
 export const RESOURCE_PATH_MOVE_TYPE = SITE_PACKAGE + "::site::ResourcePath";
+
+const LANDING_PAGE_OID = "0x2d9414edc309535bfd4cd7e80ccbc09fee18bf86b449a185b81e914096059a67";
+const FLATLAND_OID = "0xc62fae899d75705d88ef282678d17abc08a3363293def8841f0113aabd053fbb";
+const FLATLANDER_OID = "0xabf413f36aa8ba984f81f4d3e334070b351c800dacb5ea5e02d49a7621b02d96";
+export const SITES_USED_FOR_BENCHING = [
+    [LANDING_PAGE_OID, "landing page"],
+    [FLATLAND_OID, "flatland"],
+    [FLATLANDER_OID, "flatlander"],
+];
+>>>>>>> 4659319 (chore(move): upgrade to Move 2024 and publish new contract)
