3cbefe0ac49202c5c199460972f928abd8f9c1d1
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-10-08 13:53:07 +0300

    Enrich routing.test.ts for matchPathToRoute

--- portal/common/lib/routing.test.ts
+++ portal/common/lib/routing.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { getRoutes, matchPathToRoute } from "./routing";
-import { describe, test, expect } from 'vitest';
+import { test, expect } from 'vitest';
 
 const snakeSiteObjectId = '0x3e01b1b8bf0e54f7843596345faff146f1047e304410ed2eb85d5f67ad404206';
 test.skip('getRoutes', async () => {
@@ -12,13 +12,27 @@ test.skip('getRoutes', async () => {
     console.log(routes)
 });
 
-test('matchPathToRoute', () => {
-    const routes = {
-        routes_list: new Map<string, string>([
-            ['/path/*', '/index.html']
-        ])
-    };
-    const match = matchPathToRoute("/path/", routes)
-    console.log('MATCH: ', match)
-    expect(match).toEqual("/index.html")
-})
+const routesExample = {
+    routes_list: new Map<string, string>([
+        ['/*', '/default.html'],
+        ['/somewhere/else', 'else.jpeg'],
+        ['/somewhere/else/*', 'star-else.gif'],
+        ['/path/to/*', '/somewhere.html'],
+    ])
+};
+
+const testCases = [
+    ["/path/to/somewhere/", "/somewhere.html"],
+    ["/somewhere/else", 'else.jpeg'],
+    ["/", "/default.html"],
+    ["/somewhere", "/default.html"],
+    ["/somewhere/else/star", "star-else.gif"],
+    ["/somewhere/else/", 'star-else.gif'],
+]
+
+testCases.forEach(([requestPath, expected]) => {
+    test(`matchPathToRoute: "${requestPath}" -> "${expected}"`, () => {
+        const match = matchPathToRoute(requestPath, routesExample)
+        expect(match).toEqual(expected)
+    })
+});
