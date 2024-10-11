0170b4efb5333303a06878afea3ed41ca2386e73
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-10-11 13:47:11 +0300

    Server portal: integrate devnet fallback forwarding

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -5,6 +5,7 @@ import { getDomain, getSubdomainAndPath } from '@lib/domain_parsing'
 import { redirectToAggregatorUrlResponse, redirectToPortalURLResponse } from '@lib/redirects'
 import { getBlobIdLink, getObjectIdLink } from '@lib/links'
 import { resolveAndFetchPage } from '@lib/page_fetching'
+import { HttpStatusCodes } from '@lib/http/http_status_codes'
 
 export async function GET(req: Request) {
     const originalUrl = req.headers.get('x-original-url')
@@ -30,8 +31,18 @@ export async function GET(req: Request) {
     const requestDomain = getDomain(url)
 
     if (requestDomain == portalDomain && parsedUrl && parsedUrl.subdomain) {
-        console.log('fetching from the service worker')
-        return resolveAndFetchPage(parsedUrl)
+        const forwardToFallback = async () => {
+            return fetch(`${parsedUrl.subdomain}.${process.env.FALLBACK_DEVNET_PORTAL}`)
+        };
+        try {
+            const fetchPageResponse = await resolveAndFetchPage(parsedUrl)
+            if (fetchPageResponse.status==HttpStatusCodes.NOT_FOUND) {
+                return forwardToFallback()
+            }
+            return fetchPageResponse
+        } catch (error) {
+            return forwardToFallback()
+        }
     }
 
     const atBaseUrl = portalDomain == url.host.split(':')[0]
