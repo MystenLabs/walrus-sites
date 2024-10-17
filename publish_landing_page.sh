2284eeecbc92466c52ff1f97c0ac31dda3220ce7
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-10-17 10:25:45 +0300

    fix(publish-landing-page): update config path to sites-config.yaml

--- publish_landing_page.sh
+++ publish_landing_page.sh
@@ -13,7 +13,7 @@ mv temp-landing-page/index-sw-enabled.html temp-landing-page/index.html && \
 rm temp-landing-page/{404-page.template.html,sw.js,walrus-sites-portal-register-sw.js} && \
 echo "Publishing landing page to walrus sites..."
 ./target/release/site-builder --config \
-site-builder/assets/builder-example.yaml publish temp-landing-page/ \
+./sites-config.yaml publish temp-landing-page/ \
 > publish-result.log
 echo "Cleaning up..."
 rm -rf temp-landing-page
