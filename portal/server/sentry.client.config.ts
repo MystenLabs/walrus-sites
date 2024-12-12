72c4f4fce9eb00102bfdd31dba18703359ec495d
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-12-12 18:34:25 +0200

    refactor(portal): make sp locally deployable and vercel independent (#323)
    - Create `sentry_logger.ts`
    - Create `custom_blocklist_checker.ts`
    - Only integrate sentry and edge config upon production deployments,
    i.e. when running `pnpm run serve:prod:server`

--- portal/server/sentry.client.config.ts
+++ portal/server/sentry.client.config.ts
@@ -7,12 +7,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+if (process.env.ENABLE_SENTRY === "production") {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+        // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+        tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
-});
+        // Setting this option to true will print useful information to the console while you're setting up Sentry.
+        debug: true,
+    });
+}
