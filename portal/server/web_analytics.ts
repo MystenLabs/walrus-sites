3e81efe94936f2049d6dc3b6f2dcd3510ecbe871
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-01-14 11:23:20 +0200

    fix(portal): improve vercel web analytics event sampling

--- portal/server/web_analytics.ts
+++ portal/server/web_analytics.ts
@@ -3,27 +3,39 @@
 
 import { NextRequest } from "next/server";
 import { track } from "@vercel/analytics/server";
+import { getSubdomainAndPath } from "@lib/domain_parsing";
+import { config } from "configuration_loader";
 
 export async function send_to_web_analytics(request: NextRequest) {
-    // Extract various details from the request
-    const trackingData = extract_tracking_data(request)
-
     // Track only when the request is for an HTML page.
     // This is to avoid tracking requests for static assets like images, css, etc.
     // Cuts down costs since we are tracking less events.
-    const originalUrl = request.headers.get("x-original-url");
-    if (originalUrl?.endsWith('.html')) {
-        await track('route-access', trackingData)
+    const parsedUrl = getSubdomainAndPath(
+        request.nextUrl,
+        Number(config.portalDomainNameLength)
+    );
+
+    // Extract various details from the request
+    const custom_event_properties = extract_custom_event_properties(request, parsedUrl?.subdomain)
+
+    if (parsedUrl?.path?.endsWith('.html')) {
+        try {
+            await track('pageview', custom_event_properties)
+        } catch (e) {
+            console.warn("Could not track event: ", e);
+        }
     }
 }
 
-function extract_tracking_data(request: NextRequest): CustomEventProperties {
+function extract_custom_event_properties(request: NextRequest, subdomain?: string): CustomEventProperties {
     return {
         originalUrl: request.headers.get("x-original-url") || "Unknown",
+        subdomain
     };
 }
 
 // As of this writing, vercel pro plan supports at most 2 custom event props.
 type CustomEventProperties = {
     originalUrl: string;
+    subdomain?: string;
 };
