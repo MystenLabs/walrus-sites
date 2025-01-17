e7d03260df44f2bf7f1e6b49c5707152d3d490b0
giac-mysten <124184614+giac-mysten@users.noreply.github.com>
2025-01-17 14:17:04 +0100

    fix(portal): service worker uninstall (#359)
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    Co-authored-by: Alexandros Tzimas <alex.tzimas@mystenlabs.com>

--- portal/server/public/walrus-sites-sw.js
+++ portal/server/public/walrus-sites-sw.js
@@ -1,15 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .getRegistrations()
-    .then(function (registrations) {
-      registrations.forEach(function (registration) {
-        registration.unregister();
-      });
-    })
-    .catch(function (error) {
-        console.error("Error unregistering service workers:", error);
-    });
-}
+// Inspired by https://github.com/NekR/self-destroying-sw
+self.addEventListener("install", (_event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (_event) => {
+    self.registration
+        .unregister()
+        .then(() => self.clients.matchAll())
+        .then((clients) => {
+            clients.forEach((client) => {
+                if (client.url && "navigate" in client) {
+                    client.navigate(client.url);
+                }
+            });
+        });
+});
