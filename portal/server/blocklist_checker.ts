427111b38341298f77ea49c6ec06638cdf6422ce
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-01-28 17:03:45 +0200

    Create BlocklistCheckerFactory

--- portal/server/blocklist_checker.ts
+++ portal/server/blocklist_checker.ts
@@ -7,6 +7,33 @@ import { config } from 'configuration_loader';
 import assert from 'assert';
 
 /**
+ * Supported blocklist storage backends.
+ *
+ * - VercelEdgeConfig: Looks up a Vercel Edge Config db. Use this for portal deployments on Vercel.
+ * - Redis: Provides the flexibility of implementing the blocklist in any platform that can integrate
+ * a  Redis database.
+ */
+enum StorageVariant {
+    VercelEdgeConfig = "vercelEdgeConfig",
+    Redis = "redis",
+}
+
+/**
+* Creates a blocklist checker instance based on the provided storage variant.
+*/
+class BlocklistCheckerFactory {
+    static createBlocklistChecker(variant: StorageVariant): BlocklistChecker {
+        switch (variant) {
+            case StorageVariant.VercelEdgeConfig:
+                return new VercelEdgeConfigBlocklistChecker();
+            case StorageVariant.Redis:
+                // TODO: Implement Redis blocklist checker.
+                throw new Error("Redis blocklist checker is not implemented yet.");
+        }
+    }
+}
+
+/**
  * Checks domains/IDs against Vercel's Edge Config blocklist
  *
  * Validates whether a given identifier is present in the blocklist.
@@ -16,8 +43,12 @@ class VercelEdgeConfigBlocklistChecker implements BlocklistChecker {
     constructor() {
         assert(
             config.enableBlocklist,
-            "Blocklist checker should not be created if blocklist is disabled."
+            "ENABLE_BLOCKLIST variable is set to `false`."
         );
+        assert(
+            config.edgeConfig,
+            "EDGE_CONFIG variable is missing."
+        )
     }
 
     async check(id: string): Promise<boolean> {
@@ -27,6 +58,8 @@ class VercelEdgeConfigBlocklistChecker implements BlocklistChecker {
 
 let blocklistChecker: BlocklistChecker | undefined;
 if (config.enableBlocklist) {
-    blocklistChecker = new VercelEdgeConfigBlocklistChecker();
+    blocklistChecker = BlocklistCheckerFactory.createBlocklistChecker(
+        StorageVariant.VercelEdgeConfig
+    );
 }
 export default blocklistChecker;
