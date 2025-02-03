eb7a064da6f8f6bbd32ae830f1d997b685803ba7
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-02-03 19:54:09 +0200

    Add docstring

--- portal/server/src/web_analytics.ts
+++ portal/server/src/web_analytics.ts
@@ -5,6 +5,10 @@ import { NextRequest } from "next/server";
 import { track } from "@vercel/analytics/server";
 import { isHtmlPage, extractCustomEventProperties } from "./utils";
 
+/**
+* Sends a page view event to Vercel Web Analytics.
+* @param request - The incoming request to the portal.
+*/
 export async function sendToWebAnalytics(request: NextRequest) {
     if (isHtmlPage(request)) {
         try {
