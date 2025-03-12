5e26a9359ca6ddd0602c1dd81215dbe9be9bc6c0
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-03-12 17:53:28 +0200

    Fix

--- portal/server/app/route.ts
+++ portal/server/app/route.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import main from "src/main";
-import { Request } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
 	return main(req)
 }
