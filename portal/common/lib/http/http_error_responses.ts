5af660c531bc19f3cd97bea1fa439d73913e773a
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2025-03-13 11:51:35 +0200

    Fix error page for Bun - should parse with type text

--- portal/common/lib/http/http_error_responses.ts
+++ portal/common/lib/http/http_error_responses.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import template_404 from "../../html_templates/404-page.template.html";
-import hash_mismatch from "../../html_templates/hash-mismatch.html";
+import template_404 from "../../html_templates/404-page.template.html" with { type: "text" };
+import hash_mismatch from "../../html_templates/hash-mismatch.html" with { type: "text" };;
 import { HttpStatusCodes } from "./http_status_codes";
 
 const mainNotFoundErrorMessage = "You have reached the end of the internet, please turn back!"
