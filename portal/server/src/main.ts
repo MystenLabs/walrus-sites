661d3c91bd35caa46a32673a7243287997c0eecb
vlegakis <vassilis.legakis@mystenlabs.com>
2025-03-20 17:58:42 +0200

    Test

--- portal/server/src/main.ts
+++ portal/server/src/main.ts
@@ -14,6 +14,7 @@ import { instrumentationFacade } from "@lib/instrumentation"
 
 
 export default async function main(req: Request) {
+
     const url = new URL(req.url);
 
     // Send the page view event to either Amplitude or Vercel Web Analytics.
