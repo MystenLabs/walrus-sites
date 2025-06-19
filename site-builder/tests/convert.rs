e2e60b92ad7141717d67398c3c87a716c3cf73c7
nikos-terzo <nikos.terzo@mystenlabs.com>
2025-06-19 17:52:28 +0300

    Move site-builder configuration step inside cluster initialization

--- site-builder/tests/convert.rs
+++ site-builder/tests/convert.rs
@@ -2,67 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod localnode;
-use std::fs::File;
 
 use localnode::WalrusSitesClusterState;
-use site_builder::{args::GeneralArgs, config::Config, run};
+use site_builder::args::GeneralArgs;
 use sui_types::base_types::ObjectID;
-use tempfile::TempDir;
-use walrus_test_utils::WithTempDir;
 
 // Important: For tests to pass, the system they are running on need to have walrus installed.
 #[tokio::test]
 async fn converts_random_site_id() -> anyhow::Result<()> {
     let cluster = WalrusSitesClusterState::new().await?;
-    let temp_dir = TempDir::new().expect("able to create a temporary directory");
-    let sites_config_path = temp_dir.path().to_path_buf().join("sites-config.yaml");
-    // TODO: This should probably be done in localnode.
-    // Config created:
-    // ```
-    // portal: ''
-    // package: 0x1399dde83b06a80b2eb65f4c529596141bb0723411ce8386d8b2fea1c4cf6f28
-    // general:
-    //   rpc_url: http://127.0.0.1:62139
-    //   wallet: /var/folders/94/jrqsqb6s7pl_225b63wygm4m0000gn/T/.tmpXxgwwQ
-    //   wallet_env: null
-    //   wallet_address: null
-    //   walrus_context: null
-    //   walrus_binary: walrus
-    //   walrus_config: null
-    //   walrus_package: null
-    //   gas_budget: 500000000
-    // staking_object: 0x992a12ab8fe6d1530bed5832c2875064a40d404c53a00357cc61ffd2cbbe8382
-    // ```
-    let config = WithTempDir {
-        inner: Config {
-            portal: "".to_string(),
-            package: cluster.walrus_sites_package_id,
-            general: GeneralArgs {
-                rpc_url: Some(cluster.sui_cluster_handle.lock().await.rpc_url()),
-                wallet: Some(cluster.admin_wallet_with_client.temp_dir.into_path()),
-                ..Default::default()
-            },
-            staking_object: Some(
-                cluster
-                    .admin_wallet_with_client
-                    .inner
-                    .sui_client()
-                    .read_client()
-                    .get_staking_object_id(),
-            ),
-        },
-        temp_dir,
-    };
-    println!(
-        "config.temp_dir: {}",
-        config.temp_dir.path().to_str().unwrap()
-    );
 
-    let mut file = File::create(sites_config_path.as_path())?;
-    serde_yaml::to_writer(&mut file, &config.inner)?;
-
-    run(
-        Some(sites_config_path),
+    site_builder::run(
+        Some(cluster.sites_config.inner.1),
         None,
         GeneralArgs::default(),
         site_builder::args::Commands::Convert {
--- site-builder/tests/publish.rs
+++ site-builder/tests/publish.rs
@@ -2,99 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod localnode;
-use std::{fs::File, num::NonZeroU32, path::PathBuf};
+use std::{num::NonZeroU32, path::PathBuf};
 
 use localnode::WalrusSitesClusterState;
-use site_builder::{
-    args::{
-        default,
-        Commands,
-        EpochArg,
-        EpochCountOrMax,
-        GeneralArgs,
-        PublishOptions,
-        WalrusStoreOptions,
-    },
-    config::Config,
-    run,
+use site_builder::args::{
+    default,
+    Commands,
+    EpochArg,
+    EpochCountOrMax,
+    GeneralArgs,
+    PublishOptions,
+    WalrusStoreOptions,
 };
-use tempfile::TempDir;
-use walrus_test_utils::WithTempDir;
+use tokio::io::AsyncBufReadExt;
 
 // Important: For tests to pass, the system they are running on need to have walrus installed.
 #[tokio::test]
 async fn snake() -> anyhow::Result<()> {
     let cluster = WalrusSitesClusterState::new().await?;
-    let temp_dir = TempDir::new().expect("able to create a temporary directory");
-    let sites_config_path = temp_dir.path().to_path_buf().join("sites-config.yaml");
-    let rpc_url = cluster.sui_cluster_handle.lock().await.rpc_url();
-    let wallet_path = cluster
-        .admin_wallet_with_client
-        .temp_dir
-        .into_path()
-        .join("wallet_config.yaml");
-    // println!("rpc_url: {rpc_url}");
-    // println!("config.temp_dir: {}", wallet_path.to_str().unwrap());
-
-    let read_client = cluster
-        .admin_wallet_with_client
-        .inner
-        .sui_client()
-        .read_client();
-
-    let walrus_config = read_client.contract_config();
-    let temp_dir = TempDir::new().expect("able to create a temporary directory");
-    let walrus_config_path = temp_dir
-        .path()
-        .to_path_buf()
-        .join("walrus_client_config.yaml");
-    println!("walrus_config_path: {walrus_config_path:?}");
-    let mut walrus_yaml_file = File::create(walrus_config_path.as_path())?;
-    serde_yaml::to_writer(&mut walrus_yaml_file, &walrus_config)?;
-
-    // TODO: This should probably be done in localnode.
-    // Config created:
-    // ```
-    // portal: ''
-    // package: 0x1399dde83b06a80b2eb65f4c529596141bb0723411ce8386d8b2fea1c4cf6f28
-    // general:
-    //   rpc_url: http://127.0.0.1:62139
-    //   wallet: /var/folders/94/jrqsqb6s7pl_225b63wygm4m0000gn/T/.tmpXxgwwQ
-    //   wallet_env: null
-    //   wallet_address: null
-    //   walrus_context: null
-    //   walrus_binary: walrus
-    //   walrus_config: null
-    //   walrus_package: null
-    //   gas_budget: 500000000
-    // staking_object: 0x992a12ab8fe6d1530bed5832c2875064a40d404c53a00357cc61ffd2cbbe8382
-    // ```
-    let config = WithTempDir {
-        inner: Config {
-            portal: "".to_string(),
-            package: cluster.walrus_sites_package_id,
-            general: GeneralArgs {
-                rpc_url: Some(rpc_url),
-                wallet: Some(wallet_path),
-                walrus_config: Some(walrus_config_path),
-                ..Default::default()
-            },
-            staking_object: Some(read_client.get_staking_object_id()),
-        },
-        temp_dir,
-    };
-
-    let mut file = File::create(sites_config_path.as_path())?;
-    serde_yaml::to_writer(&mut file, &config.inner)?;
-
     let directory = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .unwrap()
         .join("examples")
         .join("snake");
     let ws_resources = directory.join("ws-resources.json");
-    run(
-        Some(sites_config_path),
+
+    tokio::io::BufReader::new(tokio::io::stdin())
+        .read_line(&mut String::new())
+        .await
+        .unwrap();
+
+    site_builder::run(
+        Some(cluster.sites_config.inner.1),
         None,
         GeneralArgs::default(),
         Commands::Publish {
