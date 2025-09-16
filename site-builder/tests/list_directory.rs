0c874a2cbcbf6d5271d17ba7e13dd105ac956c46
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-09-16 15:56:45 +0300

    chore(site-builder): list_directory integration test
    Extra: Add snake directory to gitignore We don't edit that directory anyways but whenever we test
    something the entries inside the snake example are modified by the site-builder resulting in
    accidental commits every now an then.

---
+++ site-builder/tests/list_directory.rs
@@ -0,0 +1,75 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::{fs, path::PathBuf};
+
+use fastcrypto::hash::{HashFunction, Sha256};
+use move_core_types::u256::U256;
+use site_builder::args::{Commands, EpochCountOrMax};
+use walrus_sdk::core::BlobId;
+
+#[allow(dead_code)]
+mod localnode;
+use localnode::{
+    args_builder::{ArgsBuilder, PublishOptionsBuilder},
+    TestSetup,
+};
+
+#[tokio::test]
+#[ignore]
+async fn publish_snake_with_list_directory() -> anyhow::Result<()> {
+    const SNAKE_FILES_UPLOAD_FILES: usize = 4;
+    let cluster = TestSetup::start_local_test_cluster().await?;
+    let directory = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("examples")
+        .join("snake");
+
+    let og_ws_resources = directory.join("ws-resources.json");
+    // Create a temp file copy so the original doesn't get mutated during the test.
+    let temp_dir = tempfile::tempdir()?;
+    let temp_ws_resources = temp_dir.path().join("ws-resources.json");
+    fs::copy(&og_ws_resources, &temp_ws_resources)?;
+
+    let args = ArgsBuilder::default()
+        .with_config(Some(cluster.sites_config_path().to_owned()))
+        .with_command(Commands::Publish {
+            publish_options: PublishOptionsBuilder::default()
+                .with_directory(directory)
+                .with_ws_resources(Some(temp_ws_resources))
+                .with_list_directory(true)
+                .with_epoch_count_or_max(EpochCountOrMax::Max)
+                .build()?,
+            site_name: None,
+        })
+        .build()?;
+    site_builder::run(args).await?;
+
+    let site = cluster.last_site_created().await?;
+    let resources = cluster.site_resources(*site.id.object_id()).await?;
+
+    assert_eq!(resources.len(), SNAKE_FILES_UPLOAD_FILES + 1); // +1 because we use a temp
+                                                               // ws-resources
+    for resource in resources {
+        let data = cluster.read_blob(&BlobId(resource.blob_id.0)).await?;
+        let mut hash_function = Sha256::default();
+        hash_function.update(&data);
+        let resource_hash: [u8; 32] = hash_function.finalize().digest;
+        assert_eq!(resource.blob_hash, U256::from_le_bytes(&resource_hash));
+
+        // Make sure that the ignored files in ws-resources.json are not published.
+        let ignored_paths = [
+            "/secret.txt",
+            "/private/data.txt",
+            "/private/nested/hidden.doc",
+        ];
+        for ignored_path in &ignored_paths {
+            assert_ne!(resource.path, *ignored_path);
+        }
+        // Make sure that the produced index.html from the list-directory command does
+        // not include paths to the ignored files of ws-resources.json
+    }
+
+    Ok(())
+}
