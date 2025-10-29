6fc4e9d0a5a375614434f0cb422ab75c3d44b8c6
nikos-terzo <nikos.terzo@mystenlabs.com>
2025-10-29 13:44:29 +0200

    Add DeployQuilts command under the flag

--- site-builder/src/lib.rs
+++ site-builder/src/lib.rs
@@ -111,6 +111,51 @@ async fn run_internal(
                 .run()
                 .await?
         }
+        #[cfg(feature = "quilts-experimental")]
+        Commands::DeployQuilts {
+            publish_options,
+            site_name,
+            object_id,
+        } => {
+            // These two flags have no relevance in Quilts.
+            let (watch, check_extend) = (false, false);
+
+            // Load the ws-resources file, to check for the site-object-id. If it exists, it means
+            // the site is already deployed, in which case we should do update the site.
+            // If it doesn't exist, we can publish a new site.
+            let (ws_resources, _) = load_ws_resources(
+                publish_options.walrus_options.ws_resources.as_deref(),
+                publish_options.directory.as_path(),
+            )?;
+
+            // if `object_id` is Some use it, else use the one from the ws-resources file
+            let site_object_id =
+                object_id.or_else(|| ws_resources.as_ref().and_then(|res| res.object_id));
+
+            let (identifier, continuous_editing, blob_management) = match site_object_id {
+                Some(object_id) => (
+                    Some(object_id),
+                    ContinuousEditing::from_watch_flag(watch),
+                    BlobManagementOptions { check_extend },
+                ),
+                None => (
+                    None,
+                    ContinuousEditing::Once,
+                    BlobManagementOptions::no_status_check(),
+                ),
+            };
+
+            SiteEditor::new(context, config)
+                .with_edit_options(
+                    publish_options,
+                    identifier,
+                    site_name,
+                    continuous_editing,
+                    blob_management,
+                )
+                .run_quilts()
+                .await?
+        }
         Commands::Publish {
             publish_options,
             site_name,
