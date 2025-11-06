0f0c2e6370d8a4975496a23ca6c04db2eebe20ff
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-06 10:50:23 -0800

    Fix stray punctuation in test comment

--- ts-sdk-site-builder/tests/flows.test.ts
+++ ts-sdk-site-builder/tests/flows.test.ts
@@ -1,17 +1,18 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SuiClient } from "@mysten/sui/client";
 import { publish_site_flow } from "../src/flows";
 import { describe, it, expect } from "bun:test";
+import { SiteBuilder } from "../src/site-builder";
+import { Transaction } from "@mysten/sui/transactions";
+import { join } from "path";
 
 describe("site publishing", () => {
     it("should publish a test site", async () => {
-        const suiClient = new SuiClient({
-            network: "testnet",
-            url: "https://fullnode.testnet.sui.io",
-        });
-        const errors = await publish_site_flow(suiClient);
-        expect(errors).toBeUndefined()
+        const configPath = join(process.cwd(), "..", "sites-config.yaml");
+        const siteBuilder = new SiteBuilder(configPath);
+        const tx = new Transaction();
+        // FIXME: expecting to be undefined for a happy path is weird. .
+        expect(await publish_site_flow(tx, siteBuilder)).toBeUndefined();
     });
 });
