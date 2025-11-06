ab4bb49eea1eaa19fbf2e5bbe5516ae713cc5760
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-06 14:50:30 -0800

    Add comments to flow tests

--- ts-sdk-site-builder/tests/flows.test.ts
+++ ts-sdk-site-builder/tests/flows.test.ts
@@ -9,10 +9,13 @@ import { join } from "path";
 
 describe("site publishing", () => {
 	it("should publish a test site", async () => {
+		// Read the config
 		const configPath = join(process.cwd(), "..", "sites-config.yaml");
+		// Initialise the SiteBuilder (orchestrator) class
 		const siteBuilder = new SiteBuilder(configPath);
+		// Create an empty transaction. It will be used to add consecutive moveCalls to it.
 		const tx = new Transaction();
-		// FIXME: expecting to be undefined for a happy path is weird. .
+		// FIXME: expecting to be undefined for a happy path is weird.
 		const tx2 = publish_site_flow(tx, siteBuilder);
         expect(await siteBuilder.run(tx2)).toBeUndefined();
     });
