ab5b7877413410312e5d4402ef0e206033411aab
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-11-06 11:43:12 -0800

    Move loadSitesConfig to utils/file_io

--- ts-sdk-site-builder/utils/file_io.ts
+++ ts-sdk-site-builder/utils/file_io.ts
@@ -4,3 +4,12 @@
 // TODO: here should be included the logic of reading files/resources.
 // TODO: files should be also be possible to be loaded from the browser.
 // TODO: configuration file loading logic (e.g. ws-resources) should be included.
+import { parseSitesConfig, type SitesConfig } from "../utils/sites_config_parser";
+import { readFileSync } from "fs";
+import { parse as parseYaml } from "yaml";
+
+export function loadSitesConfig(path: string): SitesConfig {
+    const fileContent = readFileSync(path, "utf-8");
+    const yamlData = parseYaml(fileContent);
+    return parseSitesConfig(yamlData);
+}
