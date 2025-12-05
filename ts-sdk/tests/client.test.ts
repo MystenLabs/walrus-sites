80632427cd8fab3e36250bb92f60695a2b3199a5
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-12-05 15:51:26 +0200

    Enhance test with assertions

--- ts-sdk/tests/client.test.ts
+++ ts-sdk/tests/client.test.ts
@@ -1,18 +1,24 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { expect, it, xit, describe } from 'bun:test'
+import { expect, it, describe } from 'bun:test'
 import { SuiClient } from '@mysten/sui/client'
 import { walrus } from '@mysten/walrus'
 import { walrusSites } from '@client'
 
+const NETWORK = "testnet"
+const TESTNET_RPC_URL = "https://fullnode.testnet.sui.io:443"
+
 describe('walrusSitesClientShouldBeInitialisable', () => {
-    it('initialises the walrus sites client happy path', () => {
+    it('initialises the walrus sites client', () => {
         const client = new SuiClient({
-            url: 'https://fullnode.testnet.sui.io:443'
+            network: NETWORK,
+            url: TESTNET_RPC_URL
         })
-        client
-            .$extend(walrus())
-            .$extend(walrusSites())
+        const extendedClientWithWalrus = client.$extend(walrus({ network: NETWORK }))
+        const extendedClientWithWalrusAndWalrusSites = extendedClientWithWalrus.$extend(walrusSites())
+        expect(extendedClientWithWalrusAndWalrusSites.base.network).toEqual(NETWORK)
+        expect(extendedClientWithWalrusAndWalrusSites.walrus).toBeDefined()
+        expect(extendedClientWithWalrusAndWalrusSites.walrus_sites).toBeDefined()
     })
 })
