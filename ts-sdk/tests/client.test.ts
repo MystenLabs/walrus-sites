06d67f8af3426ee33983be62cec03fa613244722
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-12-05 14:55:29 +0200

    First iteration of walrus sites client definition

---
+++ ts-sdk/tests/client.test.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { expect, it, xit, describe } from 'bun:test'
+
+describe('walrusSitesClientShouldBeInitialisable', () => {
+    xit('initialises the walrus sites client using defaults', () => {
+        //TODO
+    })
+})
