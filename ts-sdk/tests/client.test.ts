9ebc4e38cc4c4328922ad51d0a1de4d2b52e712f
Alexandros Tzimas <tzalex@outlook.com.gr>
2025-12-12 11:11:47 +0200

    Update prettierrc to wrap long lines automatically

--- ts-sdk/tests/client.test.ts
+++ ts-sdk/tests/client.test.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { expect, it, describe } from 'bun:test'
+import { expect, it, describe, beforeEach } from 'bun:test'
 import { SuiClient } from '@mysten/sui/client'
-import { walrus } from '@mysten/walrus'
-import { walrusSites } from '@client'
+import { type ClientWithExtensions } from '@mysten/sui/experimental'
+import { walrus, WalrusClient } from '@mysten/walrus'
+import { walrusSites, WalrusSitesClient } from '@client'
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519'
 
 const NETWORK = 'testnet'
@@ -12,12 +13,17 @@ const EPOCHS = 2
 const TESTNET_RPC_URL = 'https://fullnode.testnet.sui.io:443'
 
 describe('walrusSitesClientShouldBeInitialisable', () => {
-    it('initialises the walrus sites client', () => {
-        const client = new SuiClient({
+    let client: ClientWithExtensions<
+        { [x: string]: WalrusSitesClient },
+        ClientWithExtensions<{ walrus: WalrusClient }, SuiClient>
+    >
+
+    beforeEach(() => {
+        const baseClient = new SuiClient({
             network: NETWORK,
             url: TESTNET_RPC_URL,
         })
-        const extendedClientWithWalrus = client.$extend(
+        const extendedClientWithWalrus = baseClient.$extend(
             walrus({
                 uploadRelay: {
                     host: 'https://upload-relay.testnet.walrus.space',
@@ -28,29 +34,19 @@ describe('walrusSitesClientShouldBeInitialisable', () => {
                 network: NETWORK,
             })
         )
-        const extendedClientWithWalrusAndWalrusSites =
-            extendedClientWithWalrus.$extend(walrusSites())
-        expect(extendedClientWithWalrusAndWalrusSites.base.network).toEqual(NETWORK)
-        expect(extendedClientWithWalrusAndWalrusSites.walrus).toBeDefined()
-        expect(extendedClientWithWalrusAndWalrusSites.walrus_sites).toBeDefined()
+        client = extendedClientWithWalrus.$extend(walrusSites())
+    })
+
+    it('initialises the walrus sites client', () => {
+        expect(client.base.network).toEqual(NETWORK)
+        expect(client.walrus).toBeDefined()
+        expect(client.walrus_sites).toBeDefined()
     })
 
     // Note: To run this test you should specify a big timeout. Otherwise, the test
     // will fail with a timeout error. To run it use this command:
     // $ bun test --test-name-pattern publishesASmallSite --timeout 120000
     it('publishesASmallSite', async () => {
-        // Setup client
-        const client = new SuiClient({
-            network: NETWORK,
-            url: TESTNET_RPC_URL,
-        })
-        const extendedClientWithWalrus = client.$extend(walrus({ network: NETWORK }))
-        const extendedClientWithWalrusAndWalrusSites =
-            extendedClientWithWalrus.$extend(walrusSites())
-        expect(extendedClientWithWalrusAndWalrusSites.base.network).toEqual(NETWORK)
-        expect(extendedClientWithWalrusAndWalrusSites.walrus).toBeDefined()
-        expect(extendedClientWithWalrusAndWalrusSites.walrus_sites).toBeDefined()
-
         // Prepare
         const files = [
             { path: 'file1.txt', contents: new TextEncoder().encode('<div>AAAFirst</div>') },
@@ -69,7 +65,7 @@ describe('walrusSitesClientShouldBeInitialisable', () => {
             },
         }
         // Execute
-        let res = await extendedClientWithWalrusAndWalrusSites.walrus_sites?.publish(
+        let res = await client.walrus_sites?.publish(
             {
                 files,
                 siteOptions,
